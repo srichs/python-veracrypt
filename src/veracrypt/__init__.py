2902b01c5bcbfaebd97176fbe38765101cc5608f
srichs <13246896+srichs@users.noreply.github.com>
2026-02-08 18:39:21 +0000

    Fix ruff lint errors

--- src/veracrypt/__init__.py
+++ src/veracrypt/__init__.py
@@ -1,1 +1,9 @@
-from .veracrypt import Encryption, Hash, FileSystem, VeraCryptError, VeraCrypt
+from .veracrypt import (
+    Encryption as Encryption,
+    FileSystem as FileSystem,
+    Hash as Hash,
+    VeraCrypt as VeraCrypt,
+    VeraCryptError as VeraCryptError,
+)
+
+__all__ = ["Encryption", "FileSystem", "Hash", "VeraCrypt", "VeraCryptError"]
